56004b0b4ccbc035fcfc8e5ff1dfd7a42e568303
Kunal Sagar <sagar.kunal@yahoo.com>
2020-03-28 13:27:23 +0530

    conditional jsx

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Person from './Person/Person';
 
 const App = props => {
 
+  const [showState, setShowState] = useState(false);
+
   const [personsState, setPersonsState] = useState({
     persons: [
       { name: "Kunal", age: "31" },
@@ -36,6 +38,10 @@ const App = props => {
     });
   }
 
+  const togglePersons = () => {
+    setShowState(!showState);
+  }
+
   const style = {
     backgroundColor: 'white',
     font: 'inherit',
@@ -46,19 +52,23 @@ const App = props => {
   return (
     <div className="App">
         {/*Below is not recommended*/}
-        <button style={style} onClick={() => switchStateHandler('Krs')} >Switch Names</button>
-        <Person
-          // below is recommended!!!
-          onClick={switchStateHandler.bind(this, 'Kunal Sagar...')}
-          name={personsState.persons[0].name} 
-          age={personsState.persons[0].age} />
-        <Person 
-          changed={nameChangeHandler}
-          name={personsState.persons[1].name} 
-          age={personsState.persons[1].age} />
-        <Person 
-          name={personsState.persons[2].name} 
-          age={personsState.persons[2].age} />
+        <button style={style} onClick={togglePersons} >Toggle Persons</button>
+        { showState?
+            <div>
+              <Person
+                // below is recommended!!!
+                onClick={switchStateHandler.bind(this, 'Kunal Sagar...')}
+                name={personsState.persons[0].name} 
+                age={personsState.persons[0].age} />
+              <Person 
+                changed={nameChangeHandler}
+                name={personsState.persons[1].name} 
+                age={personsState.persons[1].age} />
+              <Person 
+                name={personsState.persons[2].name} 
+                age={personsState.persons[2].age} />
+            </div> : null
+          }
     </div>
   );
 }
